2ef2e7fae8c812a6485831028d0c57daab5cc320
Cheruiyot Robert <kiruir38@gmail.com>
2024-05-20 20:06:42 +0300

    complete

--- lib/an_assertion_error.py
+++ lib/an_assertion_error.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
-assert(1 == 2)
+# assert(1 == 2)
+# lib/an_assertion_error.py
+
+def test_values():
+    assert(1 == 2), "Assertion failed: 1 is not equal to 2"
+
+try:
+    test_values()
+except AssertionError as e:
+    print(e)  # This will print "Assertion failed: 1 is not equal to 2"
